6a818f08ec7deebf9c49e46ff5f718224f67b1eb
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-03 18:33:08 -0300

    wait explict

--- wait_exp.py
+++ wait_exp.py
@@ -3,11 +3,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 driver=webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 
 driver.get("https://expedia.com/")
+driver.maximize_window()
 driver.implicitly_wait(4)
 
 driver.find_element(By.XPATH,"//*[@id='tab-flight-tab-hp']").click()
@@ -21,7 +23,9 @@ driver.find_element(By.XPATH,"/html/body/meso-native-marquee/section/div/div/div
 
 driver.find_element(By.XPATH,'/html/body/meso-native-marquee/section/div/div/div[1]/section/div/div[2]/div[2]/section[1]/form/div[8]/label/button').click()
 
-wait=webdriverwait()
+#explict wait line
+wait=WebDriverWait(driver,10)
+wait.until(EC.element_to_be_clickable.By.XPATH("//*[@id='tab-flight-tab-hp']"))
 
 
 time.sleep(3)
