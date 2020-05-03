352652b07b47f87f2219013a459dc48a976db50b
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-03 19:03:58 -0300

    2nd test with more examples

--- explict_2.py
+++ explict_2.py
@@ -1,5 +1,6 @@
-#explicit wait 2
-
+#explicit wait 2 practice
+#working test_1
+'''
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,8 +9,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 driver.maximize_window()
-
+#get web page
 driver.get("https://www.amazon.ca")
+#implicit wait
 driver.implicitly_wait(5)
 #actions on search bar
 search_bar=driver.find_element(By.ID,"twotabsearchtextbox")
@@ -22,4 +24,16 @@ wait=WebDriverWait(driver,10)
 element=wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='p_85/5690392011']/span/a/div/label/i")))
 element.click()
 time.sleep(2)
-driver.quit()
+driver.quit()'''
+
+from selemnium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.support import expected_conditions as EC
+from selenium.common.support.ui import WebDriverWait
+import time
+
+#automation through firefox
+driver=webdriver.Firefox(executable_path='/usr/local/bin/geakodriver')
+#get website in this case amazon
+
