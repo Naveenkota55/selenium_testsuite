bc9d1e46524c83c56ad225ca10b34c6c2bc82da9
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-03 22:43:08 -0300

    to get number of present in webpage by tag/anchor

---
+++ links_test.py
@@ -0,0 +1,12 @@
+#working on links
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+
+driver.get("https://www.edureka.co/blog/keyboard-mouse-events-actions-class")
+all_links=driver.find_elements(By.TAG_NAME,"a")
+#to get totel number of links
+print(len(all_links))
