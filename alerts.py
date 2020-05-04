1b2cabf3eef123eb99a95f139ea60642efbd4ae5
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-04 19:02:32 -0300

    alerts dismiss

--- alerts.py
+++ alerts.py
@@ -8,5 +8,6 @@ driver.get("http://testautomationpractice.blogspot.com")
 driver.find_element_by_xpath("//*[@id='HTML9']/div[1]/button").click()
 time.sleep(2)
 
-driver.switch_to_alert().accept()
+#driver.switch_to_alert().accept()
+driver.switch_to_alert().dismiss()
 time.sleep(2)
