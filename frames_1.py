a341765eb77f13ea2922454fbb21fd8f9e2c1dcc
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-04 19:53:42 -0300

    swiching between frames with description

--- frames_1.py
+++ frames_1.py
@@ -6,21 +6,21 @@ import time
 
 driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 
-driver.get("https://www.selenium.dev/selenium/docs/api/java/")
+driver.get("https://www.selenium.dev/selenium/docs/api/java/")  #get website
 driver.implicitly_wait(3)
 
-driver.switch_to.frame("packageListFrame")
-driver.find_element_by_link_text("org.openqa.selenium").click()
+driver.switch_to.frame("packageListFrame")  #switching to first_frame
+driver.find_element_by_link_text("org.openqa.selenium").click() 
+
+driver.switch_to.default_content()  #swiching to default frame
+
+driver.switch_to.frame("packageFrame")   #swich to second frame
+driver.find_element_by_link_text("Rotatable").click()
 
 driver.switch_to.default_content()
 
-driver.switch_to.frame("packageFrame")
-driver.find_element_by_link_text("Rotatable").click()
-
-
-
 time.sleep(3)
-driver.switch_to.frame("classFrame")
+driver.switch_to.frame("classFrame")#switch to third frame
 driver.find_element(By.XPATH,"/html/body/div[2]/div[2]/ul[1]/li[5]/a").click()
 
 
