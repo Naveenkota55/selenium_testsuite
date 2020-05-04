c2532937503ec09975f5a27d3098f3e61bf0ce7a
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-04 19:04:25 -0300

    alerts

--- alerts.py
+++ alerts.py
@@ -1,3 +1,4 @@
+#contains code to accept and dismiss the alert by using sithch to alert
 from selenium import webdriver
 import time
 
@@ -8,6 +9,6 @@ driver.get("http://testautomationpractice.blogspot.com")
 driver.find_element_by_xpath("//*[@id='HTML9']/div[1]/button").click()
 time.sleep(2)
 
-#driver.switch_to_alert().accept()
-driver.switch_to_alert().dismiss()
+#driver.switch_to_alert().accept()   #to accept
+driver.switch_to_alert().dismiss()  #to dismiss
 time.sleep(2)
