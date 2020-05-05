9dc1f9990268908c1cd5709feef047e8c653d7d3
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-04 21:38:39 -0300

    html_table_partial_col

--- html_table.py
+++ html_table.py
@@ -6,3 +6,10 @@ driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 driver.get("http://testautomationpractice.blogspot.com")
 
 rows=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr"))
+print("number of rows in table =",rows)
+
+coloums=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr[1]/th"))
+print("number of rows in table =",coloums)
+
+
+driver.quit()
