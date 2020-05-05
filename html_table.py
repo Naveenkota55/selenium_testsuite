ba6be94fac5f31a7ce20dc2b25c664c7736bd5a9
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-04 21:44:10 -0300

    html_table_part_contents

--- html_table.py
+++ html_table.py
@@ -11,5 +11,8 @@ print("number of rows in table =",rows)
 coloums=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr[1]/th"))
 print("number of rows in table =",coloums)
 
-
+for r in range(1,rows+1):
+    for c in range(1,coloums+1):
+        key=driver.find_element(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr["+str(r)+"]/th["+str(c)+"]").text
+        print(key)
 driver.quit()
