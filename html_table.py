531ee02208ee2b9abc44215d98bd3742f9b4a5ba
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-04 21:52:30 -0300

    html_table_contents_added_description

--- html_table.py
+++ html_table.py
@@ -1,3 +1,5 @@
+#this script contains how to read table contents, find rows &coloums
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -5,13 +7,13 @@ driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 
 driver.get("http://testautomationpractice.blogspot.com")
 
-rows=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr"))
+rows=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr"))#to find number of rows
 print("number of rows in table =",rows)
 
-coloums=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr[1]/th"))
+coloums=len(driver.find_elements(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr[1]/th"))#to find number of columns
 print("number of rows in table =",coloums)
 
-for r in range(2,rows+1):
+for r in range(2,rows+1):#to print the table for header we can just use regular print command, below code just retrives table contents
     for c in range(1,coloums+1):
         key=driver.find_element(By.XPATH,"//*[@id='HTML1']/div[1]/table/tbody/tr["+str(r)+"]/td["+str(c)+"]").text
         print(key,end="      ")
