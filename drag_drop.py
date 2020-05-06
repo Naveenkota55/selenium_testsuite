69be7c48a39570233b623cc612a97396827df66c
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-06 20:21:10 -0300

    working_script_description added

--- drag_drop.py
+++ drag_drop.py
@@ -1,3 +1,5 @@
+#following script contains actions related to drag and drop
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -6,9 +8,9 @@ import time
 driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
 driver.maximize_window()
 driver.get("http://testautomationpractice.blogspot.com")
-src_element=driver.find_element(By.XPATH,"//*[@id='draggable']")
+src_element=driver.find_element(By.XPATH,"//*[@id='draggable']")#get the source and destination
 dest_element=driver.find_element(By.XPATH,"//*[@id='droppable']")
 actions=ActionChains(driver)
-actions.drag_and_drop(src_element,dest_element).perform()
+actions.drag_and_drop(src_element,dest_element).perform()#use drag and drop in action chain
 time.sleep(5)
 driver.close()
