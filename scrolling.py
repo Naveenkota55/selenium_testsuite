3e4dc328a50cd0dd01bf4d6fea7295577e2d520a
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-06 18:27:49 -0300

    scrolling

--- scrolling.py
+++ scrolling.py
@@ -3,9 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
-driver.maximize_window()
 driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html")
-
 driver.execute_script("window.scrollBy(0,1000)","")
-
-driver.quit()
+flag=driver.find_element(By.XPATH,"//*[@id='content']/div[2]/div[2]/table[1]/tbody/tr[35]/td[1]/img")
+driver.execute_script("arguments[0].scrollIntoView();",flag)
