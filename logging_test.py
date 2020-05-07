eea86d43d0d2c813ee8dc7d5250d255cc436cd03
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-07 20:33:32 -0300

    modfified error, added desprition

--- logging_test.py
+++ logging_test.py
@@ -1,8 +1,9 @@
+#following script to enable logging
 from selenium import webdriver
-import logging
+import logging              #module for logging
 
 logging.basicConfig(filename="/Users/naveenkota/Documents/selenium/logtest.log",
-level=logging.debug)
+level=logging.DEBUG)                        #set location and level of log 
 logging.debug("this is debug message")
 logging.info("this is info")
 logging.warning("this is warning")
