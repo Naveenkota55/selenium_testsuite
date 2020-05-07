b333a63096280733bca6c44de4d10b00dddc001e
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-07 20:31:17 -0300

    added test location

--- logging_test.py
+++ logging_test.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import logging
 
+logging.basicConfig(filename="/Users/naveenkota/Documents/selenium/logtest.log",
+level=logging.debug)
 logging.debug("this is debug message")
 logging.info("this is info")
-logging.warning("thi is warning")
+logging.warning("this is warning")
 logging.error("this is error message")
 logging.critical("this is critical")
