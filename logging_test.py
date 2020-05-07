e91907a2f3c441d274c00676b417b8d07b297272
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-07 20:28:26 -0300

    logging messgages test

---
+++ logging_test.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import logging
+
+logging.debug("this is debug message")
+logging.info("this is info")
+logging.warning("thi is warning")
+logging.error("this is error message")
+logging.critical("this is critical")
