c6ee320f954d9c6398c088d98c49261cf83b3af7
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-08 02:14:28 -0300

    unit_test 2 with modules and class unittest cases*optimized and more test cases added

--- unittest_2.py
+++ unittest_2.py
@@ -7,7 +7,7 @@ def setUpModule():
 def tearDownModule():
     print("end of module")
 class searchengine(unittest.TestCase):
-    driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+    
     
     @classmethod
     def setUp(self):
@@ -24,18 +24,36 @@ class searchengine(unittest.TestCase):
     @classmethod
     def setUpClass(hi):
         print("start of class")
-
+    
     def test_google(self):
-        #self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
         self.driver.get("http://google.com")
         print(self.driver.title)
         self.driver.close()
         
     def test_bing(self):
-        #self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
         self.driver.get("https://bing.com")
         print(self.driver.title)
-     #   self.driver.close()
+        self.driver.close()
+
+    def test_wiki(self):
+        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        self.driver.get("http://wikipedia.com")
+        print(self.driver.title)
+        self.driver.close()
+    
+    def test_yahoo(self):
+        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        self.driver.get("https://yahoo.co.in")
+        print(self.driver.title)
+        self.driver.close()
+    
+    def test_duck(self):
+        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        self.driver.get("https://duckduckgo.com/")
+        print(self.driver.title)
+        self.driver.close()
 if __name__=="__main__":
     unittest.main()
         
