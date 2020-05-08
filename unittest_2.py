9b6e59b5273e436d8b0d49dda821a1e0e7462821
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-08 01:45:11 -0300

    unit_test 2 with modules and class unittest cases

--- unittest_2.py
+++ unittest_2.py
@@ -2,22 +2,40 @@ import unittest
 from selenium import webdriver
 import time
 
-
+def setUpModule():
+    print("start of module")
+def tearDownModule():
+    print("end of module")
 class searchengine(unittest.TestCase):
+    driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+    
+    @classmethod
     def setUp(self):
         print("this is setup")
-        
+
+    @classmethod
+    def tearDown(self):
+        print("this is teardown")
+    
+    @classmethod
+    def tearDownClass(hi):
+        print("end of class")
+    
+    @classmethod
+    def setUpClass(hi):
+        print("start of class")
+
     def test_google(self):
-        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        #self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
         self.driver.get("http://google.com")
         print(self.driver.title)
         self.driver.close()
         
     def test_bing(self):
-        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        #self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
         self.driver.get("https://bing.com")
         print(self.driver.title)
-        self.driver.close()
+     #   self.driver.close()
 if __name__=="__main__":
     unittest.main()
         
