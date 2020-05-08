c79cc47ed3301c76d00fac6cfd2fbee102471d68
Naveenkota55 <bhargavkota55@gmail.com>
2020-05-08 00:39:44 -0300

    unit_test 2 testing

--- unittest_2.py
+++ unittest_2.py
@@ -1,16 +1,20 @@
 import unittest
 from selenium import webdriver
+import time
+
 
 class searchengine(unittest.TestCase):
-    def test_bing(self):
-        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
-        self.driver.get("https://www.bing.com")
-        print(self.driver.title)
-        self.driver.close()
     def test_google(self):
         self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
-        self.driver.get("https://www.google.com")
+        self.driver.get("http://google.com")
+        print(self.driver.title)
+        self.driver.close()
+        
+    def test_bing(self):
+        self.driver=webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+        self.driver.get("https://bing.com")
         print(self.driver.title)
         self.driver.close()
 if __name__=="__main__":
     unittest.main()
+        
